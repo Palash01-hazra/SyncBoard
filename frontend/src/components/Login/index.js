5f2da7d0260c0c3dfa107cfebaac97a452ca953f
Palash Hazra <140879786+Palash01-hazra@users.noreply.github.com>
2025-09-10 12:30:36 +0530

    Update index.js

--- frontend/src/components/Login/index.js
+++ frontend/src/components/Login/index.js
@@ -13,7 +13,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://localhost:5000/api/users/login', {
+      const response = await fetch('https://syncboard-backend-7zx2.onrender.com/api/users/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
