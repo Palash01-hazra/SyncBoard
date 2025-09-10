0a46631a34297c4bf235b67400dd4395560f7c98
Palash Hazra <140879786+Palash01-hazra@users.noreply.github.com>
2025-09-10 12:34:05 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 // utils/api.js
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000/api/canvas"; 
+const API_BASE_URL = "https://syncboard-backend-7zx2.onrender.com/api/canvas"; 
 
 const token = localStorage.getItem('whiteboard_user_token')
 const canvasId = localStorage.getItem('canvas_id')
