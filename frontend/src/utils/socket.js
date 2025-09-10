951e8c2d778d8efee54bca54292b8331b5dc3ba2
Palash Hazra <140879786+Palash01-hazra@users.noreply.github.com>
2025-09-10 12:34:42 +0530

    Update socket.js

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 const token = localStorage.getItem("whiteboard_user_token");
 
-const socket = io("http://localhost:5000", {
+const socket = io("https://syncboard-backend-7zx2.onrender.com", {
   extraHeaders: token ? { Authorization: `Bearer ${token}` } : {}, // Only send if token exists
 });
 
